cf570233f944939d448abb326c8dcdece5fcc41e
sunericd <edsun@DESKTOP-5BCOF0K.localdomain>
2023-08-31 18:57:09 -0700

    packaging

---
+++ tissue/__init__.py
@@ -0,0 +1,3 @@
+'''TISSUE (Transcript Imputation with Spatial Single-cell Uncertainty Estimation) provides tools for estimating well-calibrated uncertainty measures for gene expression predictions in single-cell spatial transcriptomics datasets and utilizing them in downstream analyses'''
+
+__version__ = "0.0.1"
